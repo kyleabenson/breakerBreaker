475c093b45384c01f64981ebc6997bafd725a9d4
Kyle Benson <kyleabenson@gmail.com>
2024-12-13 18:13:42 +0000

    use doc k8s definition

--- maint.tf
+++ maint.tf
@@ -37,17 +37,54 @@ resource "google_project_iam_member" "gemini_for_google_cloud_user" {
   member  = var.user_id
 }
 
-resource "google_container_cluster" "primary" {
-  name               = "my-gke-cluster"
-  location           = "us-central1-a" # Replace with your desired location
-  initial_node_count = 1
+resource "google_compute_network" "default" {
+  name = "example-network"
 
-  node_config {
-    machine_type = "e2-medium" # Or another machine type
-    disk_size    = 30          # In GB
+  auto_create_subnetworks  = false
+  enable_ula_internal_ipv6 = true
+}
+
+resource "google_compute_subnetwork" "default" {
+  name = "example-subnetwork"
+
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "us-central1"
+
+  stack_type       = "IPV4_IPV6"
+  ipv6_access_type = "INTERNAL" # Change to "EXTERNAL" if creating an external loadbalancer
+
+  network = google_compute_network.default.id
+  secondary_ip_range {
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.0.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.1.0/24"
   }
 }
 
+resource "google_container_cluster" "default" {
+  name = "example-autopilot-cluster"
+
+  location                 = "us-central1"
+  enable_autopilot         = true
+  enable_l4_ilb_subsetting = true
+
+  network    = google_compute_network.default.id
+  subnetwork = google_compute_subnetwork.default.id
+
+  ip_allocation_policy {
+    stack_type                    = "IPV4_IPV6"
+    services_secondary_range_name = google_compute_subnetwork.default.secondary_ip_range[0].range_name
+    cluster_secondary_range_name  = google_compute_subnetwork.default.secondary_ip_range[1].range_name
+  }
+
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
 
 output "investigation_url" {
      value = format("%s/%s","https://console.cloud.google.com/troubleshooting/investigations/list?project=",var.gcp_project_id)
